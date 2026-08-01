2e6a6b627f32ca54cc4e3616302b8943d690cf60
AmirMustafa10 <amirmustafa0001@gmail.com>
2026-08-01 23:03:17 +0300

    feat(accounts): configure Django admin interface

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,70 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import User, Profile
 
-admin.site.register(User)
-admin.site.register(Profile)
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    pass
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    @admin.display(ordering="user__username", description="Username")
+    def username(self, obj):
+        return obj.user.username
+
+    list_display = (
+        "username",
+        "location",
+        "available_for_work",
+    )
+
+    list_display_links = ("username",)
+
+    search_fields = (
+        "user__username",
+        "location",
+    )
+
+    list_filter = ("available_for_work",)
+
+    ordering = ("user__username",)
+
+    list_select_related = ("user",)
+
+    list_per_page = 25
+
+    fieldsets = (
+        (
+            "User Identity",
+            {
+                "fields": (
+                    "user",
+                    "bio",
+                    "location",
+                    "available_for_work",
+                ),
+            },
+        ),
+        (
+            "Media & Documents",
+            {
+                "fields": (
+                    "avatar",
+                    "resume",
+                ),
+            },
+        ),
+        (
+            "Social & Portfolio Links",
+            {
+                "classes": ("collapse",),  # Makes this section collapsible in admin
+                "fields": (
+                    "github_url",
+                    "linkedin_url",
+                    "website_url",
+                ),
+            },
+        ),
+    )
