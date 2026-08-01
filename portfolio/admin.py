98c781108b075253be3253b346470dc246e1a9b9
AmirMustafa10 <amirmustafa0001@gmail.com>
2026-08-02 00:09:18 +0300

    feat(portfolio): configure skill admin

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Skill
 
-# Register your models here.
+
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "icon",
+    )
+
+    list_display_links = ("name",)
+
+    search_fields = ("name",)
+    
+    ordering = ("name",)
+    
+    list_per_page = 25
+
