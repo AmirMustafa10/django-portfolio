55ad64e7ac3aa3e68da1bb3430c3858f860d5ca0
AmirMustafa10 <amirmustafa0001@gmail.com>
2026-08-06 22:18:00 +0300

    feat(accounts): add authentication views and templates

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import login
+from .forms import LoginForm, RegisterForm
 
-# Create your views here.
+
+def register_view(request):
+
+    if request.user.is_authenticated:
+        return redirect("home")
+
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+
+            login(request, user)
+
+            return redirect("home")
+
+    else:
+        form = RegisterForm()
+
+    return render(
+        request,
+        "accounts/register.html",
+        {"form": form},
+    )
+
+
+def login_view(request):
+
+    if request.user.is_authenticated:
+        return redirect("home")
+
+    if request.method == "POST":
+        form = LoginForm(request, data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+
+            return redirect("home")
+
+    else:
+        form = LoginForm()
+
+    return render(
+        request,
+        "accounts/login.html",
+        {"form": form},
+    )
