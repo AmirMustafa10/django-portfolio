eeafa2bf7b9f8f94759faa3546a67dd6adaac875
AmirMustafa10 <amirmustafa0001@gmail.com>
2026-08-07 23:51:37 +0300

    feat(auth): improve login and base UI

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .views import login_view, register_view
+from .views import login_view, register_view, logout_view
 
 app_name = "accounts"
 
@@ -8,4 +8,5 @@ app_name = "accounts"
 urlpatterns = [
     path("register/", register_view, name="register"),
     path("login/", login_view, name="login"),
+    path("logout/", logout_view, name="logout"),
 ]
