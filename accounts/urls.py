ebc8b0b933b04570f37549485ceaf7b0d395a46c
AmirMustafa10 <amirmustafa0001@gmail.com>
2026-08-08 03:00:09 +0300

    feat(developers): add developers page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .views import login_view, register_view, logout_view
+from .views import login_view, register_view, logout_view, developers_view
 
 app_name = "accounts"
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path("register/", register_view, name="register"),
     path("login/", login_view, name="login"),
     path("logout/", logout_view, name="logout"),
+    path("developers/", developers_view, name="developers"),
 ]
