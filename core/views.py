cd9b6068d6ca1843bcf5e8864c45a132a47f3367
AmirMustafa10 <amirmustafa0001@gmail.com>
2026-08-10 23:53:26 +0300

    feat(core): improve homepage and base navigation

--- core/views.py
+++ core/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from accounts.models import Profile
+from django.db.models import Count
 
 
 def home(request):
-    return render(request, "home.html")
+
+    developers = (
+        Profile.objects.select_related("user")
+        .prefetch_related("skills")
+        .annotate(
+            projects_count=Count("projects", distinct=True),
+        )[:6]
+    )
+
+    return render(
+        request,
+        "home.html",
+        {
+            "developers": developers,
+        },
+    )
