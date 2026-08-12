30cf9a5088fd5c0e827c78b1f23b037a59298393
AmirMustafa10 <amirmustafa0001@gmail.com>
2026-08-12 03:33:43 +0300

    feat(accounts): add profile edit view, url and templates

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,12 @@
-from django.contrib import admin
 from django.urls import path
-from .views import login_view, register_view, logout_view, developers_view, developer_detail_view
+from .views import (
+    login_view,
+    register_view,
+    logout_view,
+    developers_view,
+    developer_detail_view,
+    edit_profile_view,
+)
 
 app_name = "accounts"
 
@@ -11,4 +17,5 @@ urlpatterns = [
     path("logout/", logout_view, name="logout"),
     path("developers/", developers_view, name="developers"),
     path("developer/<str:username>/", developer_detail_view, name="developer_detail"),
+    path("edit_profile/", edit_profile_view, name="edit_profile"),
 ]
