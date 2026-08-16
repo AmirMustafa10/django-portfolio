ae1f54c73b1d5acb3bed42745afa49b0c9c3594b
AmirMustafa10 <amirmustafa0001@gmail.com>
2026-08-16 22:17:56 +0300

    feat(dashboard): add dashboard and activity views and templates

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home, dashboard_view, activity_view
 
 urlpatterns = [
     path("", home, name="home"),
+    path("dashboard/", dashboard_view, name="dashboard"),
+    path("activity/", activity_view, name="activity"),
 ]
